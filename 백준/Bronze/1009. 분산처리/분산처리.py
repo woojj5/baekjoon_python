7a2240dd5cb19e69f4de984395472e47a30cfc2b
woojj5 <34640735+woojj5@users.noreply.github.com>
2024-05-29 19:49:39 +0900

    [Bronze II] Title: 분산처리, Time: 80 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -0,0 +1,9 @@
+temp = int(input())
+#print(arr)
+for i in range(temp):
+    a,b = map(int, input().split())
+    if a % 10 == 0:
+        print(10)
+    else:
+        b = b%4 +4
+        print(a**b %10)
