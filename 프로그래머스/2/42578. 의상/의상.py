3a541714828cbfaf4d5a5deaa09988f7d7ffe1a5
woojj5 <34640735+woojj5@users.noreply.github.com>
2024-09-05 10:49:07 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,10 +1,10 @@
 def solution(clothes):
     dicta = {}
-    for i in clothes:
-        if i[1] not in dicta:
-            dicta[i[1]] = 1
+    for _,i in clothes:
+        if i not in dicta:
+            dicta[i] = 1
         else:
-            dicta[i[1]] += 1
+            dicta[i] += 1
     answer = 1
     for i in dicta.values():
         answer = (answer) * (i+1)
