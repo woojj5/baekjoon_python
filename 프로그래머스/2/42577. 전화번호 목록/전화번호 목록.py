af02cbf888d42c1fcc2b4b86449c2d676ea22781
woojj5 <34640735+woojj5@users.noreply.github.com>
2024-09-05 10:35:22 +0900

    [level 2] Title: 전화번호 목록, Time: 91.07 ms, Memory: 30.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,6 +1,7 @@
 def solution(phone_book):
+    answer = True
     phone_book.sort()
-    for i in range(len(phone_book)-1):
-        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
-            return False   
-    return True
+    for i,j in zip(phone_book, phone_book[1:]):
+        if j.startswith(i):
+            answer = False
+    return answer
