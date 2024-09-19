a56770124f8e47d22db8279284ce599ecb545233
woojj5 <34640735+woojj5@users.noreply.github.com>
2024-09-19 13:37:41 +0900

    [level 3] Title: 섬 연결하기, Time: 0.13 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+def solution(n, costs):
+    answer = 0
+    costs.sort(key = lambda x: x[2])
+    tree = set([0])
+    while len(tree) < n:
+        for c in costs:
+            if len(set(c[:2]) & tree) == 1:
+                tree.update(c[:2])
+                answer+=c[2]
+                break
+    return answer
