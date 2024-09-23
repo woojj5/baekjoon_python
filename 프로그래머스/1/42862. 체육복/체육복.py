8e4519e1337fa70b9304a0ee6cc27f5f77e8862b
woojj5 <34640735+woojj5@users.noreply.github.com>
2024-09-23 18:19:21 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,20 +1,22 @@
 def solution(n, lost, reserve):
     answer = 0
-    shirt = [1] * (n+1)
+    uniform = [1] * (n+1)
+    uniform[0] = 0
     for i in lost:
-        shirt[i]-=1
+        uniform[i]-=1
     for i in reserve:
-        shirt[i]+=1
+        uniform[i]+=1
+   # print(uniform)
     for i in range(1, n+1):
-        if shirt[i] == 0:
-            if i -1 >0 and shirt[i-1] > 1:
-                shirt[i] =1
-                shirt[i-1] =1
-            elif i +1 <n+1 and shirt[i+1] > 1:
-                shirt[i] =1
-                shirt[i+1] =1
-    for i in shirt:
-        if i >0:
+        if uniform[i] == 0:
+            if i-1 > 0 and uniform[i-1] == 2:
+                uniform[i-1] = 1
+                uniform[i] = 1
+            elif i+1 < n+1 and uniform[i+1] == 2:
+                uniform[i+1] = 1
+                uniform[i] = 1
+    #print(uniform)
+    for i in uniform:
+        if i > 0:
             answer+=1
-    answer-=1
     return answer
