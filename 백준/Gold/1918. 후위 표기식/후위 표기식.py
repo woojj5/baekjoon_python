ec2351e9b92947837574f5fd1bc37f692954fab0
woojj5 <34640735+woojj5@users.noreply.github.com>
2024-10-07 14:54:49 +0900

    [Gold II] Title: 후위 표기식, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.py
@@ -0,0 +1,24 @@
+lsisa = input()
+st  = []
+ans = ''
+for i in lsisa:
+    if i.isalpha():
+        ans +=i
+    else:
+        if i == '(':
+            st.append(i)
+        elif i == '*' or i == "/":
+            while st and (st[-1] == "*" or st[-1] == '/'):
+                ans +=  st.pop()
+            st.append(i)
+        elif i == "+" or i == "-":
+            while st and st[-1] != "(":
+                ans +=  st.pop()
+            st.append(i)
+        elif i == ")":
+            while st and st[-1] != "(":
+                ans +=  st.pop()
+            st.pop()
+while st:
+    ans+= st.pop()
+print(ans)
