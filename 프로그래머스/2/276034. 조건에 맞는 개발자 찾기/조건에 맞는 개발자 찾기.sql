0e37bdf653b1da19f5e66d375a2fb04f641c0aaf
woojj5 <34640735+woojj5@users.noreply.github.com>
2024-10-11 17:05:19 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,2 @@
+-- 코드를 작성해주세요
+select ID, EMAIL, FIRST_NAME, LAST_NAME from DEVELOPERS where SKILL_CODE & (select code from SKILLCODES  where Name = 'Python' ) or SKILL_CODE & (select code from SKILLCODES  where Name = 'C#' ) order by id asc
