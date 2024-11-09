b44e7c83e59a6edb80b386d89146ba13ad37d3bc
woojj5 <34640735+woojj5@users.noreply.github.com>
2024-11-09 14:39:38 +0900

    [Gold IV] Title: 알고스팟, Time: 132 ms, Memory: 114152 KB -BaekjoonHub

---
+++ 백준/Gold/1261. 알고스팟/알고스팟.py
@@ -0,0 +1,25 @@
+from collections import deque
+n,m = map(int, input().split())
+graph = []
+for i in range(m):
+    graph.append(list(map(int, input())))
+dist = [[-1] *n for i in range(m)]
+
+def bfs(x,y):
+    q = deque()
+    q.append([x,y])
+    dist[x][y] = 0
+    while q:
+        x,y = q.popleft()
+        for dx,dy in ((1,0), (-1, 0), (0,1), (0,-1)):
+            nx = x + dx
+            ny = y + dy
+            if 0<= nx < m and 0<= ny < n and dist[nx][ny] == -1:
+                if graph[nx][ny] == 0:
+                    dist[nx][ny] = dist[x][y]
+                    q.appendleft([nx, ny])
+                else:
+                    dist[nx][ny] = dist[x][y] + 1
+                    q.append([nx, ny])
+bfs(0,0)
+print(dist[m-1][n-1])
