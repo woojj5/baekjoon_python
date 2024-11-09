75f2eba25a024da242a4033c7c1cfb3d291dfdbc
woojj5 <34640735+woojj5@users.noreply.github.com>
2024-11-09 14:42:33 +0900

    [Gold IV] Title: 알고스팟, Time: 124 ms, Memory: 114128 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.py
+++ 백준/Gold/1261. 알고스팟/알고스팟.py
@@ -15,11 +15,8 @@ def bfs(x,y):
             nx = x + dx
             ny = y + dy
             if 0<= nx < m and 0<= ny < n and dist[nx][ny] == -1:
-                if graph[nx][ny] == 0:
-                    dist[nx][ny] = dist[x][y]
-                    q.appendleft([nx, ny])
-                else:
-                    dist[nx][ny] = dist[x][y] + 1
-                    q.append([nx, ny])
+                if graph[nx][ny] == 0: q.appendleft([nx, ny])
+                else: q.append([nx, ny])
+                dist[nx][ny] = dist[x][y] + graph[nx][ny]
 bfs(0,0)
 print(dist[m-1][n-1])
