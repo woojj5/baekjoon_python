b23804f29adbc70e45116fdfc403cf70dc3e8c87
woojj5 <34640735+woojj5@users.noreply.github.com>
2024-11-23 14:48:45 +0900

    [Silver III] Title: 계단 오르기, Time: 112 ms, Memory: 109628 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -3,19 +3,12 @@ n = int(input())
 arr = []
 for i in range(n):
     arr.append(int(input()))
-res = copy.deepcopy(arr)
-if n == 1:
-    print(arr[0])
-elif n == 2:
-    print(arr[0] + arr[1])
+if n <= 2:
+    print(sum(arr))
 else:
-    # 점화식 초기값 설정
+    res = copy.deepcopy(arr)
     res[1] = arr[0] + arr[1]
     res[2] = max(arr[0], arr[1]) + arr[2]
-
-    # 일반 점화식
-    for i in range(3, n):
-        res[i] = max(res[i - 3] + arr[i - 1], res[i - 2]) + arr[i]
-
-    # 결과 출력
-    print(res[n - 1])
+    for i in range(3,n):
+        res[i] = max(res[i-3] + arr[i-1], res[i-2]) + arr[i]
+    print(res[n-1])
