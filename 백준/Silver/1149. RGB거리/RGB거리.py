7b1a336e8b452e5a860e539d0046eee1357ee106
woojj5 <34640735+woojj5@users.noreply.github.com>
2024-11-23 14:01:50 +0900

    [Silver I] Title: RGB거리, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,18 +1,10 @@
-iter = int(input())
-arr = [[0 for _ in range(3)] for i in range(iter)]
-choice = [[0 for _ in range(3)] for i in range(iter)]
-for i in range(iter):
-    length = list(map(int, input().split()))
-    for j in range(len(length)):
-        arr[i][j] = length[j]
-choice[0][0] = arr[0][0]
-choice[0][1] = arr[0][1]
-choice[0][2] = arr[0][2]
-
-
-for i in range(1, iter):
-    choice[i][0] = min(choice[i-1][1],choice[i-1][2]) +  arr[i][0]
-    choice[i][2] = min(choice[i-1][1],choice[i-1][0]) +  arr[i][2]
-    choice[i][1] = min(choice[i-1][0],choice[i-1][2]) +  arr[i][1]
-
-print(min(choice[iter-1]))
+n = int(input())
+arr = []
+for i in range(n):
+    arr.append(list(map(int, input().split())))
+for i in range(1, n):
+    arr[i][0] = min(arr[i-1][1], arr[i-1][2]) + arr[i][0]
+    arr[i][1] = min(arr[i-1][0], arr[i-1][2]) + arr[i][1]
+    arr[i][2] = min(arr[i-1][0], arr[i-1][1]) + arr[i][2]
+cnt = min(arr[n-1][1], arr[n-1][0], arr[n-1][2])
+print(cnt)
