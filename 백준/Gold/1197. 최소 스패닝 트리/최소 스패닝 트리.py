1eb24ea845ea0772c1c1d1097a8c0f337a6f4737
woojj5 <34640735+woojj5@users.noreply.github.com>
2024-11-26 14:14:16 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 368 ms, Memory: 131588 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,38 +1,27 @@
-import sys
-input = sys.stdin.readline
-
-V, E = map(int, input().split())
-lista = [i for i in range(V + 1)]
-rank = [0] * (V + 1)  # 유니온 바이 랭크를 위해 랭크 배열 생성
+n,m = map(int, input().split())
+arr = [i for i in range(n+1)]
+visit =  [0] * (n+1)
 elist = []
-
-for _ in range(E):
+for i in range(m):
     elist.append(list(map(int, input().split())))
-elist.sort(key=lambda x: x[2])
+elist.sort(key= lambda  x: x[2])
 
-# 경로 압축을 적용한 find 함수
 def find(x):
-    if x != lista[x]:
-        lista[x] = find(lista[x])  # 경로 압축
-    return lista[x]
+    if arr[x] != x:
+        arr[x] = find(arr[x])
+    return arr[x]
 
-# 유니온 바이 랭크를 적용한 union 함수
-def union(x, y):
-    rootX = find(x)
-    rootY = find(y)
-    if rootX != rootY:
-        if rank[rootX] > rank[rootY]:
-            lista[rootY] = rootX
-        elif rank[rootX] < rank[rootY]:
-            lista[rootX] = rootY
-        else:
-            lista[rootY] = rootX
-            rank[rootX] += 1
+def union(x,y):
+    x = find(x)
+    y = find(y)
+    if x< y:
+        arr[x] = y
+    else:
+        arr[y] = x
 
 ans = 0
-for s, e, w in elist:
-    if find(s) != find(e):
-        union(s, e)
-        ans += w
-
+for v,e,w in elist:
+    if find(v) != find(e):
+        union(v,e)
+        ans+=w
 print(ans)
