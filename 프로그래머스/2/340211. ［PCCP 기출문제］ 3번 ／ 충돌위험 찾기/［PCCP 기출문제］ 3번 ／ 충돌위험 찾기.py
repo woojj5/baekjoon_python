5f8e7b448bb31f7aecfade2f92a55dd2005a6405
woojj5 <34640735+woojj5@users.noreply.github.com>
2024-12-16 17:54:25 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 576.28 ms, Memory: 176 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.py
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.py
@@ -4,34 +4,32 @@ def solution(points, routes):
         idx = 0
         pa = []
         for i in range(len(route)-1):
-            sx,sy = spot[route[i]-1]
-            ex,ey = spot[route[i+1]-1]
-            
+            sx,sy = arr[route[i]-1]
+            ex,ey = arr[route[i+1]-1]
+        
             while sx != ex:
-                pa.append((sx, sy, idx))
-                if sx< ex:
+                pa.append((sx,sy, idx))
+                if sx < ex:
                     sx+=1
                 else:
                     sx-=1
                 idx+=1
             while sy != ey:
-                pa.append((sx, sy, idx))
-                if sy< ey:
+                pa.append((sx,sy, idx))
+                if sy < ey:
                     sy+=1
                 else:
                     sy-=1
                 idx+=1
         pa.append((sx, sy, idx))
         return pa
-                    
-    spot = [i for i in points]
+    answer = 0
+    arr = [i for i in points]    
     second = []
     for route in routes:
         second.extend(bfs(route))
-    res = 0
     temp = Counter(second)
     for i in temp.values():
-        if i >= 2:
-            res+=1
-    
-    return res
+        if i>=2:
+            answer+=1
+    return answer
