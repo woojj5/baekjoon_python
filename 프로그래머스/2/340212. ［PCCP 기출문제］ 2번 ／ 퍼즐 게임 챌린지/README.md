6c4c7362cf7ae1e5926db5a159b0df44dc894101
woojj5 <34640735+woojj5@users.noreply.github.com>
2024-12-16 15:53:12 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 1740.91 ms, Memory: 35 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/README.md
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/README.md
@@ -1,10 +1,10 @@
 # [level 2] [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지 - 340212 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340212?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340212) 
 
 ### 성능 요약
 
-메모리: 34.7 MB, 시간: 1793.64 ms
+메모리: 35 MB, 시간: 1740.91 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 09일 19:06:31
+2024년 12월 16일 15:53:09
 
 ### 문제 설명
 
--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.py
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.py
@@ -1,20 +1,18 @@
 def solution(diffs, times, limit):
-    l = 1
-    r = max(diffs)
     answer = max(diffs)
-    while l< r:
-        mid = int((l + r) /2)
-        time = times[0]
-        
+    l,r = 1, max(diffs)
+    while l<r:
+        m = int((l+r)//2)
+        s = times[0]
         for i in range(1, len(diffs)):
             k = 0
-            if mid < diffs[i]:
-                k = diffs[i] - mid
-            time+=  (times[i] + times[i-1]) * k + times[i]
-            
-        if time <= limit:
-            r = mid
-            answer = mid
-        else:
-            l = mid+1
+            if m<diffs[i]:
+                k = diffs[i] - m
+            s+= ((times[i]+ times[i-1]) * (k)+ times[i])
+        if s<=limit:
+            r = m
+            answer = m
+        elif s>= limit:
+            l = m+1
     return answer
+                
