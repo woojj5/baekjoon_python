8e94e6510db485a8e4658f9db4a9db12df1e43d6
woojj5 <34640735+woojj5@users.noreply.github.com>
2024-12-18 16:18:30 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 355.11 ms, Memory: 14.2 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -1,33 +1,33 @@
 from collections import deque
-
 def solution(land):
     n = len(land)
     m = len(land[0])
-    result = [0 for i in range(m+1)]
-    visit = [[0 for i in range(m)] for j in range(n)]
+    res = [0 for i in range(m+1)]
+    arr = [[0 for i in range(m)] for j in range(n)]
     def bfs(x,y):
-        q = deque()
+        q =deque()
         q.append([x,y])
-        cnt = 0
-        visit[x][y] = 1
+        arr[x][y] = 1
         min_y, max_y = y,y
+        cnt = 0
         while q:
             x,y = q.popleft()
             min_y = min(y, min_y)
             max_y = max(y, max_y)
             cnt+=1
-            for dx,dy in [(1,0), (-1,0), (0,1), (0,-1)]:
+            for dx,dy in [[1,0], [-1,0], [0,1], [0,-1]]:
                 nx = x + dx
                 ny = y + dy
-                if 0<= nx < len(land) and 0<= ny < len(land[0]) and land[nx][ny] == 1 and visit[nx][ny] == 0:
-                    q.append([nx, ny])
-                    visit[nx][ny] = 1
+                if 0<=nx<n and 0<=ny<m and arr[nx][ny] == 0 and land[nx][ny]:
+                    arr[nx][ny] = 1
+                    q.append([nx, ny])      
+    
         for i in range(min_y, max_y+1):
-            result[i]+= cnt
+            res[i]+=cnt
     answer = 0
     for i in range(n):
         for j in range(m):
-            if land[i][j] and not visit[i][j]:
+            if arr[i][j] == 0 and land[i][j]:
                 bfs(i,j)
-    answer = max(result)
+    answer = max(res)
     return answer
