10102a4084fe8841e8d997a5721f004847e755ce
woojj5 <34640735+woojj5@users.noreply.github.com>
2024-12-31 16:14:06 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 1439.67 ms, Memory: 34.9 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.py
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.py
@@ -1,18 +1,19 @@
 def solution(diffs, times, limit):
     answer = max(diffs)
     l,r = 1, max(diffs)
-    while l<r:
+    while l< r:
         m = int((l+r)//2)
         s = times[0]
         for i in range(1, len(diffs)):
             k = 0
-            if m<diffs[i]:
+            if m < diffs[i]:
                 k = diffs[i] - m
             s+= ((times[i]+ times[i-1]) * (k)+ times[i])
-        if s<=limit:
+        
+        if s<= limit:
             r = m
             answer = m
         elif s>= limit:
             l = m+1
+        
     return answer
-                
