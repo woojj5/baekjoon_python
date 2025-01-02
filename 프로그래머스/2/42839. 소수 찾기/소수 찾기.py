81cbe3753c24abbf31ec54c501c0c54b96b4f3a4
woojj5 <34640735+woojj5@users.noreply.github.com>
2025-01-02 16:23:56 +0900

    [level 2] Title: 소수 찾기, Time: 34.24 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,24 +1,26 @@
-from itertools import permutations
-def prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n**0.5)+1):
-        if n%i == 0:
-            return False
-    return True
-
+import math
 def solution(numbers):
-    answer = 0
-    numbers = list(numbers)
-    temp = []
-    
-    for i in range(1, len(numbers)+1):
-        temp += list(permutations(numbers, i)) 
-    #print(temp)
-    num = [int(''.join(t)) for t in temp] 
-    ans = []
-    for i in num:
-        if prime(i):
-            ans.append(i)
-    return len(set(ans))
-    
+    ans_list = []
+    def prime(n):
+        if n in ans_list:
+            return False
+        ans_list.append(n)
+        if n<2:
+            return False
+        for i in range(2, int(math.sqrt(n))+1):
+            if n % i == 0:
+                return False
+
+        return True
+
+    def dfs(s, others):
+        global answer
+        if s != '':
+            if prime(int(s)):
+                answer+=1
+        for i in range(len(others)):
+            dfs(s+others[i], others[:i]+others[i+1:])
+    global answer
+    answer = 0   
+    dfs("", numbers)
+    return answer
