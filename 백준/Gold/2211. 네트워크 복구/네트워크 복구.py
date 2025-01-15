fc6c006845a05431076b34d5020a924f51817caa
woojj5 <34640735+woojj5@users.noreply.github.com>
2025-01-15 13:10:11 +0900

    [Gold II] Title: 네트워크 복구, Time: 244 ms, Memory: 133600 KB -BaekjoonHub

--- 백준/Gold/2211. 네트워크 복구/네트워크 복구.py
+++ 백준/Gold/2211. 네트워크 복구/네트워크 복구.py
@@ -3,41 +3,40 @@ import heapq
 input = sys.stdin.readline
 INF = int(1e9)
 
-
 def dijkstra(start):
     q = []
-    distance[start] = 0
+    dist[start] = 0
     heapq.heappush(q, (0, start))
-
     while q:
-        dist, now = heapq.heappop(q)
-
-        if distance[now] < dist:
+        cost, cur = heapq.heappop(q)
+        if dist[cur] < cost:
             continue
-
-        for next_node, next_cost in graph[now]:
-            cost = dist + next_cost
-
-            if distance[next_node] > cost:
-                # 더 짧은 거리로 갱신될 때마다 parent도 함께 갱신
-                parent[next_node] = now
-                distance[next_node] = cost
-                heapq.heappush(q, (cost, next_node))
+        for nxt_cost, nxt in graph[cur]:
+            cst = nxt_cost + cost
+            if dist[nxt] > cst:
+                dist[nxt] = cst
+                visit[nxt] = cur
+                heapq.heappush(q, (cst, nxt))
 
 
-n, m = map(int, input().split())
-
-distance = [INF] * (n + 1)
-parent = [0] * (n + 1)
-graph = [[] for _ in range(n + 1)]
-
+n,m = map(int, input().split())
+graph = [[] for i in range(n+1)]
+dist = [INF] * (n+1)
+visit = [0] * (n+1)
 for _ in range(m):
-    a, b, c = map(int, input().split())
-    graph[a].append((b, c))
-    graph[b].append((a, c))
+    a,b,c = map(int, input().split())
+    graph[a].append((c,b))
+    graph[b].append((c,a))
 
 dijkstra(1)
-
-print(n - 1)
+print(n-1)
 for i in range(2, n + 1):
-    print(i, parent[i])
+    print(i, visit[i])
+
+
+
+
+
+
+
+    
