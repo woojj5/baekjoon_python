aeca5bec351963c52d3e1847e44f5a024824901b
woojj5 <34640735+woojj5@users.noreply.github.com>
2025-01-23 12:16:57 +0900

    [Gold IV] Title: 이분 그래프, Time: 1232 ms, Memory: 54328 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,36 +1,35 @@
-from collections import deque
+import sys
+
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+def dfs(x, group):
+    visit[x] = group
+    for i in graph[x]:
+        if not visit[i]:
+            a = dfs(i, -group)
+            if not a:
+                return False
+        elif visit[i] == visit[x]:
+            return False
+    return True
+
 k = int(input())
-def bfs(x,color):
-    q = deque()
-    q.append(x)
-    visit[x] = color
-    while q:
-        x = q.popleft()
-        for next in graph[x]:
-            if not visit[next]:
-                visit[next] = -visit[x]
-                q.append(next)
-            elif visit[next] == visit[x]:
-                return True
-    return False
-
-
-
 for _ in range(k):
     n,m = map(int, input().split())
+    graph = [[] for i in range(n+1)]
     visit = [0] * (n+1)
-    graph = [[]  for _ in range(n+1)]
-    cnt = 0
     for i in range(m):
-        x,y = map(int, input().split())
-        graph[x].append(y)
-        graph[y].append(x)
+        a,b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
     
     for i in range(1, n+1):
         if not visit[i]:
-            if bfs(i, 1):
-                print("NO")
+            res = dfs(i,1)
+            if not res:
                 break
-    else:
+    if res:
         print("YES")
-
+    else:
+        print("NO")
