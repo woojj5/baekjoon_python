a891cda08f5c5dfff9a8c275a0e9c9d8302dc4fe
woojj5 <34640735+woojj5@users.noreply.github.com>
2025-01-23 11:34:40 +0900

    [Gold IV] Title: 이분 그래프, Time: 844 ms, Memory: 202940 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,40 +1,36 @@
-import sys
-
-sys.setrecursionlimit(10 ** 6)
-input = sys.stdin.readline
-
-t = int(input())
-
-def dfs(v, color):
-    visit[v] =  color
-    for i in tree[v]:
-        if visit[i] == 0:
-            if dfs(i, -color):
-                pass
-            else:
-                return False
-        elif visit[i] == visit[v]:
-            return False
-    return True
+from collections import deque
+k = int(input())
+def bfs(x,color):
+    q = deque()
+    q.append(x)
+    visit[x] = color
+    while q:
+        x = q.popleft()
+        for next in graph[x]:
+            if not visit[next]:
+                visit[next] = -visit[x]
+                q.append(next)
+            elif visit[next] == visit[x]:
+                return True
+    return False
 
 
-for times in range(t):
-    v, e = map(int, input().split())
-    tree = [[] for i in range(v+1)]
-    visit = [0] *(v+1)
-    for i in range(e):
-        a, b = map(int, input().split())
-        tree[a].append(b)
-        tree[b].append(a)
 
-    biapartite  = True
-    for i in range(1, v+1):
-        if visit[i] == 0:
-            biapartite = dfs(i, 1)
-            if not biapartite:
+for _ in range(k):
+    n,m = map(int, input().split())
+    visit = [0] * (n+1)
+    graph = [[]  for _ in range(n+1)]
+    cnt = 0
+    for i in range(m):
+        x,y = map(int, input().split())
+        graph[x].append(y)
+        graph[y].append(x)
+    
+    for i in range(1, n+1):
+        if not visit[i]:
+            if bfs(i, 1):
+                print("NO")
                 break
-    if biapartite:
-        print("YES")
     else:
-        print("NO")
+        print("YES")
 
