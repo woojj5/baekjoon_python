9effe158151cc810c33cb6799fc3fde0ef97b690
woojj5 <34640735+woojj5@users.noreply.github.com>
2025-02-10 15:34:34 +0900

    [Silver I] Title: 오목, Time: 88 ms, Memory: 109412 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.py
+++ 백준/Silver/2615. 오목/오목.py
@@ -1,30 +1,30 @@
+board = [list(map(int, input().split())) for _ in range(19)]
+res = 0
 n = 19
-graph = [list(map(int, input().split())) for _ in range(n)]
 dx = [0,1,-1,1]
 dy = [1,1,1,0]
-chk1 = 0
-res = 0
+
 def dfs(x,y):
     global res
     for i in range(4):
         cnt = 1
-        nx,ny = x+dx[i], y+dy[i]
-        while 0<=nx<n and 0<=ny<n and graph[nx][ny] == graph[x][y]:
-            cnt+=1
+        nx = x + dx[i]
+        ny = y + dy[i]
+        while 0<=nx < n and 0<= ny < n and board[nx][ny] == board[x][y]:
             nx+=dx[i]
             ny+=dy[i]
+            cnt+=1
             if cnt == 5:
-                if not(0<=nx<n and 0<=ny<n and graph[nx][ny] == graph[x][y]):
-                    if not(0<=x-dx[i] < n and 0<=y-dy[i] < n and graph[x-dx[i]][y-dy[i]] == graph[x][y]):
-                        print(graph[x][y])
-                        print(x+1,y+1)
+                if not(0<=nx < n and 0<= ny < n and board[nx][ny] == board[x][y]):
+                    if not(0<=x-dx[i] < n and 0<= y-dy[i] < n and board[x][y] == board[x-dx[i]][y-dy[i]]):
+                        print(board[x][y])
+                        print(x+1, y+1)
                         exit(0)
-                    else: break
-                else: break
-                    
-            
+                    else:break
+                else:break
+             
 for i in range(n):
     for j in range(n):
-        if graph[i][j]:
+        if board[i][j]:
             dfs(i,j)
 print(0)
