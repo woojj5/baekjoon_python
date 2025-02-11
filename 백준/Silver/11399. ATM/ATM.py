d07392cd571dbefc735be63a88a425323380db39
woojj5 <34640735+woojj5@users.noreply.github.com>
2025-02-11 11:04:05 +0900

    [Silver IV] Title: ATM, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,9 +1,9 @@
 n = int(input())
 arr = list(map(int, input().split()))
+
 arr.sort()
-s = arr[0]
+dp = [0] * n
+dp[0] = arr[0]
 for i in range(1, n):
-	s += arr[i]
-	for j in range(0, i):
-		s += arr[j]
-print(s)
+    dp[i] = dp[i-1] + arr[i]
+print(sum(dp))
