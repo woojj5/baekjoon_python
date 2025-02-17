2abb41cd91cf7177b059dc37f622aa79df3c9169
woojj5 <34640735+woojj5@users.noreply.github.com>
2025-02-17 11:13:30 +0900

    [Silver III] Title: 피보나치 함수, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,13 +1,11 @@
-a = int(input())
-arr = [[ 0 for i in range(2)] for _ in range(40+1)]
-
+t = int(input())
+arr = [[0,0] for _ in range(41)]
 arr[0] = [1,0]
 arr[1] = [0,1]
 for i in range(2, 40+1):
-    arr[i][0] = arr[i-2][0] + arr[i-1][0]
-    arr[i][1] = arr[i-2][1] + arr[i-1][1]
-
-for i in range(a):
-    b = int(input())
-    print(arr[b][0], end = " ")
-    print(arr[b][1] , end = " ")
+    arr[i][0]= arr[i-1][0] + arr[i-2][0]
+    arr[i][1]= arr[i-1][1] + arr[i-2][1]
+for  _ in range(t):
+    a = int(input())
+    print(arr[a][0], end = " ")
+    print(arr[a][1])
