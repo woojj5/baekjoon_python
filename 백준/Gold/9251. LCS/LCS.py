cf89a7a6a0a976d1cdd12ec102b0ddb9d9cdefe7
woojj5 <34640735+woojj5@users.noreply.github.com>
2025-02-19 11:13:14 +0900

    [Gold V] Title: LCS, Time: 120 ms, Memory: 118564 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,10 +1,10 @@
-stringa = list(' '+input())
-stringb = list(' '+input())
-dp = [[0] * len(stringb) for i in range(len(stringa))]
+a  = ' '+input().strip()
+b  = ' '+input().strip()
+dp = [[0] * len(b) for _ in range(len(a))]
 
-for i in range(1, len(stringa)):
-    for j in range(1, len(stringb)):
-        if stringa[i] == stringb[j]:
+for i in range(1, len(a)):
+    for j in range(1, len(b)):
+        if a[i] == b[j]:
             dp[i][j] = dp[i-1][j-1] + 1
         else:
             dp[i][j] = max(dp[i-1][j], dp[i][j-1])
