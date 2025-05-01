2618cf38005c672ae6911dbdcf939c047acae236
woojj5 <34640735+woojj5@users.noreply.github.com>
2025-05-01 11:31:33 +0900

    [Gold III] Title: 방 번호, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/1082. 방 번호/방 번호.py
+++ 백준/Gold/1082. 방 번호/방 번호.py
@@ -3,23 +3,16 @@ input = sys.stdin.readline
 n = int(input())
 arr = list(map(int, input().split()))
 t = int(input())
-dict = [""] * 51
+dict = [""]*(51)
 for i in range(n):
     dict[arr[i]] = str(i)
-for i in range(1, t+1):
-    if dict[i] == "":
-        continue
-    for j in range(n):
-        if i + arr[j] > t:
-            continue
-        elif dict[i+arr[j]] == "":
-            dict[i+arr[j]] = dict[i] + str(j)
-            continue
-        elif int(dict[i+arr[j]]) >= int(dict[i] + str(j)):
-            continue
-        dict[i+arr[j]] = dict[i] + str(j)
-ans= 0
 for i in range(t+1):
+    for j in range(n):
+        if arr[j] + i <= t:
+            if dict[i+arr[j]]  == "": dict[i+arr[j]] = dict[i] + str(j)
+            elif int(dict[i+arr[j]]) < int(dict[i] + str(j)): dict[i+arr[j]] = dict[i] + str(j)
+ans = 0
+for i in range(1, t+1):
     if dict[i] == "":
         continue
     ans = max(ans, int(dict[i]))
